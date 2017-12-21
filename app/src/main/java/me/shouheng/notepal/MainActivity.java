3bd9ac1f67562ffefd3f176749c4b46fdcb837be
Wang Shouheng <shouheng2015@gmail.com>
2017-12-21 20:31:24 +0800

    1.Include fabric futures; 2.Add preferences demo.

--- app/src/main/java/me/shouheng/notepal/MainActivity.java
+++ app/src/main/java/me/shouheng/notepal/MainActivity.java
@@ -1,9 +1,10 @@
 package me.shouheng.notepal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import me.shouheng.notepal.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
