9621ebd71a2f731cc0c5e656ff5d7e3ad39ab1fd
shouheng88 <shouheng2015@gmail.com>
2018-01-01 12:46:25 +0800

    Add new setting options

--- app/src/main/java/me/shouheng/notepal/fragment/SettingsFragment.java
+++ app/src/main/java/me/shouheng/notepal/fragment/SettingsFragment.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceFragment;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import me.shouheng.notepal.R;
+import me.shouheng.notepal.activity.CommonActivity;
 import me.shouheng.notepal.activity.ThemedActivity;
+import me.shouheng.notepal.dialog.FeedbackDialog;
+import me.shouheng.notepal.intro.IntroActivity;
 import me.shouheng.notepal.util.ColorUtils;
+import me.shouheng.notepal.util.LogUtils;
 import me.shouheng.notepal.util.PreferencesUtils;
 import me.shouheng.notepal.widget.ColorPreference;
 
 /**
  * Created by wang shouheng on 2017/12/21.*/
 public class SettingsFragment extends PreferenceFragment {
+
+    private final static String KEY_FEEDBACK = "feedback";
+    private final static String KEY_USER_GUIDE = "user_guide";
+    private final static String KEY_USER_INTRO = "user_intro";
 
     private CheckBoxPreference isDarkTheme, coloredNavigationBar;
 
@@ -52,7 +62,7 @@ public class SettingsFragment extends PreferenceFragment {
         });
         accentColor.setOnPreferenceClickListener(preference -> {
             if (getActivity() != null && getActivity() instanceof OnPreferenceClickListener) {
-                ((OnPreferenceClickListener) getActivity()).onPreferenceClick(PreferencesUtils.DEFAULT_NOTEBOOK_COLOR);
+                ((OnPreferenceClickListener) getActivity()).onPreferenceClick(PreferencesUtils.ACCENT_COLOR);
             }
             return true;
         });
@@ -73,6 +83,32 @@ public class SettingsFragment extends PreferenceFragment {
             }
             return true;
         });
+
+        findPreference(KEY_FEEDBACK).setOnPreferenceClickListener(preference -> {
+            FeedbackDialog.newInstance(getActivity(), (dialog, feedback) ->
+                    LogUtils.d("onSend: " + feedback)
+            ).show(((CommonActivity) getActivity()).getSupportFragmentManager(), "Feedback Editor");
+            return true;
+        });
+        findPreference(KEY_USER_GUIDE).setOnPreferenceClickListener(preference -> {
+            return true;
+        });
+        findPreference(KEY_USER_INTRO).setOnPreferenceClickListener(preference -> {
+            showIntroduction();
+            return true;
+        });
+    }
+
+    private void showIntroduction() {
+        new MaterialDialog.Builder(getActivity())
+                .title(R.string.text_tips)
+                .content(R.string.show_introduction_again)
+                .positiveText(R.string.text_confirm)
+                .negativeText(R.string.text_cancel)
+                .onPositive((materialDialog, dialogAction) -> {
+                    IntroActivity.launch(getActivity());
+                })
+                .show();
     }
 
     private void updateThemeSettings() {
