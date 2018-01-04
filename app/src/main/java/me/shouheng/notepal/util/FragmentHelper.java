933c51c857f7467f8e2f4e0dc5065c56fbfdcd8b
Wang Shouheng <shouheng2015@gmail.com>
2018-01-04 18:29:50 +0800

    Enable fragment transition animations.

--- app/src/main/java/me/shouheng/notepal/util/FragmentHelper.java
+++ app/src/main/java/me/shouheng/notepal/util/FragmentHelper.java
@@ -6,6 +6,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import me.shouheng.notepal.PalmApp;
+import me.shouheng.notepal.R;
+
 /**
  * Created by wang shouheng on 2017/12/23.*/
 public class FragmentHelper {
@@ -13,19 +16,32 @@ public class FragmentHelper {
     public static void replace(AppCompatActivity activity, Fragment fragment, @IdRes int containerId) {
         FragmentManager fragmentManager = activity.getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
+        setCustomAnimations(transaction);
         transaction.replace(containerId, fragment).commit();
     }
 
     public static void replace(AppCompatActivity activity, android.app.Fragment fragment, @IdRes int containerId) {
         android.app.FragmentManager fragmentManager = activity.getFragmentManager();
         android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        setCustomAnimations(transaction);
         transaction.replace(containerId, fragment).commit();
     }
 
     public static void replaceWithCallback(AppCompatActivity activity, Fragment fragment, @IdRes int containerId) {
         FragmentManager fragmentManager = activity.getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
+        setCustomAnimations(transaction);
         transaction.addToBackStack(null);
         transaction.replace(containerId, fragment).commit();
+    }
+
+    private static void setCustomAnimations(FragmentTransaction transaction) {
+        if (!PreferencesUtils.getInstance(PalmApp.getContext()).systemAnimationEnabled()) return;
+        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down, R.animator.slide_up, R.animator.slide_down);
+    }
+
+    private static void setCustomAnimations(android.app.FragmentTransaction transaction) {
+        if (!PreferencesUtils.getInstance(PalmApp.getContext()).systemAnimationEnabled()) return;
+        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down, R.animator.slide_up, R.animator.slide_down);
     }
 }
