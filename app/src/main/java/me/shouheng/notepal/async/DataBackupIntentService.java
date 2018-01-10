62de3c05f77c09863d59de95faccd0fb1c7256d0
Wang Shouheng <shouheng2015@gmail.com>
2018-01-10 18:42:29 +0800

    The logic of importing data from external storage

--- app/src/main/java/me/shouheng/notepal/async/DataBackupIntentService.java
+++ app/src/main/java/me/shouheng/notepal/async/DataBackupIntentService.java
@@ -6,7 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import me.shouheng.notepal.R;
@@ -16,6 +23,7 @@ import me.shouheng.notepal.model.Attachment;
 import me.shouheng.notepal.provider.AttachmentsStore;
 import me.shouheng.notepal.provider.PalmDB;
 import me.shouheng.notepal.util.FileHelper;
+import me.shouheng.notepal.util.LogUtils;
 import me.shouheng.notepal.util.NotificationsHelper;
 
 /**
@@ -49,28 +57,9 @@ public class DataBackupIntentService extends IntentService {
 
         if (ACTION_DATA_EXPORT.equals(intent.getAction())) {
             exportData(intent);
+        } else if (ACTION_DATA_IMPORT.equals(intent.getAction())) {
+            importData(intent);
         }
-    }
-
-    private synchronized void exportData(Intent intent) {
-        boolean includeSettings = intent.getBooleanExtra(INTENT_BACKUP_INCLUDE_SETTINGS, false);
-        String backupName = intent.getStringExtra(INTENT_BACKUP_NAME);
-
-        File backupDir = FileHelper.getBackupDir(backupName);
-        // delete previous backup if exist
-        FileHelper.delete(this, backupDir.getAbsolutePath());
-
-        backupDir = FileHelper.getBackupDir(backupName);
-
-        exportDB(backupDir);
-
-        exportAttachments(backupDir);
-
-        if (includeSettings) exportSettings(backupDir);
-
-        String title = getString(R.string.backup_data_export_completed);
-        String text = backupDir.getPath();
-        createNotification(intent, this, title, text, backupDir);
     }
 
     private void createNotification(Intent intent, Context mContext, String title, String message, File backupDir) {
@@ -101,6 +90,28 @@ public class DataBackupIntentService extends IntentService {
         mNotificationsHelper.show();
     }
 
+    // region export data
+    private synchronized void exportData(Intent intent) {
+        boolean includeSettings = intent.getBooleanExtra(INTENT_BACKUP_INCLUDE_SETTINGS, false);
+        String backupName = intent.getStringExtra(INTENT_BACKUP_NAME);
+
+        File backupDir = FileHelper.getBackupDir(backupName);
+        // delete previous backup if exist
+        FileHelper.delete(this, backupDir.getAbsolutePath());
+
+        backupDir = FileHelper.getBackupDir(backupName);
+
+        exportDB(backupDir);
+
+        exportAttachments(backupDir);
+
+        if (includeSettings) exportSettings(backupDir);
+
+        String title = getString(R.string.backup_data_export_completed);
+        String text = backupDir.getPath();
+        createNotification(intent, this, title, text, backupDir);
+    }
+
     private boolean exportDB(File backupDir) {
         File database = getDatabasePath(PalmDB.DATABASE_NAME);
         return (FileHelper.copyFile(database, new File(backupDir, PalmDB.DATABASE_NAME)));
@@ -125,4 +136,57 @@ public class DataBackupIntentService extends IntentService {
         File preferences = FileHelper.getSharedPreferencesFile(this);
         return (FileHelper.copyFile(preferences, new File(backupDir, preferences.getName())));
     }
+    // endregion
+
+    // region import data
+    synchronized private void importData(Intent intent) {
+        String backupName = intent.getStringExtra(INTENT_BACKUP_NAME);
+        File backupDir = FileHelper.getBackupDir(backupName);
+
+        importDB(backupDir);
+
+        importAttachments(backupDir);
+
+        importSettings(backupDir);
+
+        String title = getString(R.string.backup_data_import_completed);
+        String text = getString(R.string.backup_click_to_refresh_application);
+        createNotification(intent, this, title, text, backupDir);
+    }
+
+    private boolean importDB(File backupDir) {
+        File database = getDatabasePath(PalmDB.DATABASE_NAME);
+        if (database.exists()) {
+            database.delete();
+        }
+        return (FileHelper.copyFile(new File(backupDir, PalmDB.DATABASE_NAME), database));
+    }
+
+    private void importAttachments(File backupDir) {
+        File attachmentsDir = FileHelper.getAttachmentDir(this);
+        File backupAttachmentsDir = new File(backupDir, attachmentsDir.getName());
+        if (!backupAttachmentsDir.exists()) {
+            return;
+        }
+        Collection list = FileUtils.listFiles(backupAttachmentsDir, FileFilterUtils.trueFileFilter(), TrueFileFilter.INSTANCE);
+        Iterator i = list.iterator();
+        int imported = 0;
+        File file = null;
+        while (i.hasNext()) {
+            try {
+                file = (File) i.next();
+                FileUtils.copyFileToDirectory(file, attachmentsDir, true);
+                mNotificationsHelper.setMessage(getString(R.string.text_attachment) + " " + imported++ + "/" + list.size()).show();
+            } catch (IOException e) {
+                LogUtils.e("Error importing the attachment " + file.getName());
+            }
+        }
+    }
+
+    private boolean importSettings(File backupDir) {
+        File preferences = FileHelper.getSharedPreferencesFile(this);
+        File preferenceBackup = new File(backupDir, preferences.getName());
+        return (FileHelper.copyFile(preferenceBackup, preferences));
+    }
+    // endregion
 }
