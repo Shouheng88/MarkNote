90efb2926a675324ffaea0ab187957a8c80d9670
Wang Shouheng <shouheng2015@gmail.com>
2018-01-16 12:56:35 +0800

    Add desktop shortcut app widget

--- app/src/main/java/me/shouheng/notepal/config/Constants.java
+++ app/src/main/java/me/shouheng/notepal/config/Constants.java
@@ -50,6 +50,7 @@ public class Constants {
     public final static String INTENT_WIDGET = "widget_id";
     public final static String ACTION_TAKE_PHOTO = "action_widget_take_photo";
     public final static String ACTION_WIDGET_SHOW_LIST = "action_widget_show_list";
+    public final static String ACTION_WIDGET_MAIN = "action_widget_main";
     // endregion
 
     public final static String PREFS_NAME = BuildConfig.APPLICATION_ID + "_preferences";
---
+++ app/src/main/java/me/shouheng/notepal/widget/desktop/DesktopShortcutAppWidget.java
@@ -0,0 +1,22 @@
+package me.shouheng.notepal.widget.desktop;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.util.SparseArray;
+import android.widget.RemoteViews;
+
+import me.shouheng.notepal.R;
+import me.shouheng.notepal.util.LogUtils;
+
+public class DesktopShortcutAppWidget extends WidgetProvider {
+
+    @Override
+    protected RemoteViews getRemoteViews(Context context, int widgetId, boolean isSmall, boolean isSingleLine, SparseArray<PendingIntent> pendingIntentsMap) {
+        LogUtils.d(isSingleLine + " " + isSmall);
+        RemoteViews views;
+        views = new RemoteViews(context.getPackageName(), R.layout.widget_layout_small);
+        views.setOnClickPendingIntent(R.id.main, pendingIntentsMap.get(R.id.main));
+        return views;
+    }
+}
+
