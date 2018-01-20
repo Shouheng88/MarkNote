b4a0e9370652b3a660343debdcd12c54aea26797
shouheng88 <shouheng2015@gmail.com>
2018-01-20 21:47:16 +0800

    Add basic functions of statistic

--- app/src/main/java/me/shouheng/notepal/provider/helper/StatisticsHelper.java
+++ app/src/main/java/me/shouheng/notepal/provider/helper/StatisticsHelper.java
@@ -1,11 +1,21 @@
 package me.shouheng.notepal.provider.helper;
 
 import android.content.Context;
+import android.graphics.Color;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
 import me.shouheng.notepal.config.Constants;
 import me.shouheng.notepal.model.Attachment;
 import me.shouheng.notepal.model.Location;
@@ -20,28 +30,19 @@ import me.shouheng.notepal.provider.NotebookStore;
 import me.shouheng.notepal.provider.NotesStore;
 import me.shouheng.notepal.provider.TimelineStore;
 import me.shouheng.notepal.provider.schema.TimelineSchema;
+import me.shouheng.notepal.util.LogUtils;
 import me.shouheng.notepal.util.TimeUtils;
 
 /**
  * Created by wang shouheng on 2018/1/19.*/
 public class StatisticsHelper {
 
-    public static List<Integer> getAddedStatistics(Context context, ModelType modelType) {
-        Calendar sevenDaysAgo = TimeUtils.sevenDaysAgo();
-        List<Integer> states = new LinkedList<>();
-        for (int i=0; i<7; i++) {
-            long startMillis = sevenDaysAgo.getTimeInMillis();
-            sevenDaysAgo.add(Calendar.DAY_OF_YEAR, 1);
-            long endMillis = sevenDaysAgo.getTimeInMillis();
-            String whereSQL = TimelineSchema.ADDED_TIME + " >= " + startMillis
-                    + " AND " + TimelineSchema.ADDED_TIME + " < " + endMillis
-                    + " AND " + TimelineSchema.MODEL_TYPE + " = " + modelType.id
-                    + " AND " + TimelineSchema.OPERATION + " = " + Operation.ADD.id;
-            states.add(TimelineStore.getInstance(context).getCount(whereSQL, Status.NORMAL, false));
-        }
-        return states;
-    }
-
+    /**
+     * Get the statistics.
+     *
+     * @param context the context
+     * @return the Stats object contains the actions result.
+     */
     public static Stats getStats(Context context) {
         Stats stats = new Stats();
 
@@ -91,5 +92,96 @@ public class StatisticsHelper {
         stats.setMindsStats(getAddedStatistics(context, ModelType.MIND_SNAGGING));
 
         return stats;
+    }
+
+    /**
+     * Get added models of last seven days. This will lead to query action in main thread.
+     * If you have already got the queried data, you may use {@link #getLine(List, int)} method
+     * to get the line chart data.
+     *
+     * @param context context
+     * @param modelType model type
+     * @param color line color
+     * @return the line chart data
+     */
+    public static LineChartData getLineChartData(Context context, ModelType modelType, int color) {
+        return getLineChartData(Arrays.asList(getLine(context, modelType, color)));
+    }
+
+    /**
+     * To get the line chart data use the queried data.
+     *
+     * @param lineStatistics line statistics
+     * @param color the color of line
+     * @return the line chart data
+     */
+    public static LineChartData getLineChartData(List<Integer> lineStatistics, int color) {
+        return getLineChartData(Arrays.asList(getLine(lineStatistics, color)));
+    }
+
+    private static LineChartData getLineChartData(List<Line> lines) {
+        Calendar sevenDaysAgo = Calendar.getInstance();
+        sevenDaysAgo.set(Calendar.HOUR_OF_DAY, 0);
+        sevenDaysAgo.set(Calendar.MINUTE, 0);
+        sevenDaysAgo.set(Calendar.SECOND, 0);
+        sevenDaysAgo.set(Calendar.MILLISECOND, 0);
+        sevenDaysAgo.add(Calendar.DAY_OF_YEAR, -6);
+        List<String> days = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd", Locale.getDefault());
+        for (int i=0; i<7; i++){
+            days.add(sdf.format(sevenDaysAgo.getTime()));
+            sevenDaysAgo.add(Calendar.DAY_OF_YEAR, 1);
+        }
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+        data.setAxisXBottom(null);
+        data.setAxisYLeft(null);
+        data.setBaseValue(0);
+        data.setValueLabelBackgroundColor(Color.TRANSPARENT);
+        Axis axis = Axis.generateAxisFromCollection(Arrays.asList(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f), days);
+        data.setAxisXBottom(axis);
+        return data;
+    }
+
+    private static Line getLine(Context context, ModelType modelType, int color) {
+        return getLine(StatisticsHelper.getAddedStatistics(context, modelType), color);
+    }
+
+    private static Line getLine(List<Integer> lineStatistics, int color) {
+        List<PointValue> values = new LinkedList<>();
+        int length = lineStatistics.size();
+        for (int j = 0; j < length; ++j) {
+            values.add(new PointValue(j, lineStatistics.get(j)));
+        }
+        LogUtils.d("getLineChartData: " + lineStatistics);
+
+        Line line = new Line(values);
+        line.setColor(color);
+        line.setShape(ValueShape.CIRCLE);
+        line.setCubic(false);
+        line.setFilled(true);
+        line.setHasLabels(true);
+        line.setHasLines(true);
+        line.setHasPoints(true);
+        line.setPointRadius(3);
+
+        return line;
+    }
+
+    private static List<Integer> getAddedStatistics(Context context, ModelType modelType) {
+        Calendar sevenDaysAgo = TimeUtils.sevenDaysAgo();
+        List<Integer> states = new LinkedList<>();
+        for (int i=0; i<7; i++) {
+            long startMillis = sevenDaysAgo.getTimeInMillis();
+            sevenDaysAgo.add(Calendar.DAY_OF_YEAR, 1);
+            long endMillis = sevenDaysAgo.getTimeInMillis();
+            String whereSQL = TimelineSchema.ADDED_TIME + " >= " + startMillis
+                    + " AND " + TimelineSchema.ADDED_TIME + " < " + endMillis
+                    + " AND " + TimelineSchema.MODEL_TYPE + " = " + modelType.id
+                    + " AND " + TimelineSchema.OPERATION + " = " + Operation.ADD.id;
+            states.add(TimelineStore.getInstance(context).getCount(whereSQL, Status.NORMAL, false));
+        }
+        return states;
     }
 }
