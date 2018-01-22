792fe41b5232f761b4c8a96193d926f8ca86d2f2
Wang Shouheng <shouheng2015@gmail.com>
2018-01-22 12:10:02 +0800

    modify the todo items

--- app/src/main/java/me/shouheng/notepal/PalmApp.java
+++ app/src/main/java/me/shouheng/notepal/PalmApp.java
@@ -1,13 +1,20 @@
 package me.shouheng.notepal;
 
+import android.app.Activity;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
 import org.polaric.colorful.Colorful;
 
+import me.shouheng.notepal.model.Model;
+
 
 /**
+ * TODO All the todo items in later version:
+ * 1. Add ringtone to {@link me.shouheng.notepal.async.DataBackupIntentService} when included the notification logic;
+ * 2. Enable copy link logic when the server is ready. {@link me.shouheng.notepal.util.ModelHelper#copyLink(Activity, Model)}
+ *
  * Created by wangshouheng on 2017/2/26. */
 public class PalmApp extends Application{
 
