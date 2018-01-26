64c3d1828b9bd0a5ab28c727a88da1318c9f7121
shouheng88 <shouheng2015@gmail.com>
2018-01-26 08:46:57 +0800

    notice dialog content

--- app/src/main/java/me/shouheng/notepal/dialog/NoticeDialog.java
+++ app/src/main/java/me/shouheng/notepal/dialog/NoticeDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.text.Html;
 import android.view.LayoutInflater;
 
 import me.shouheng.notepal.R;
@@ -28,6 +29,15 @@ public class NoticeDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.dialog_notice, null, false);
+
+        binding.tv1.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part1)));
+        binding.tv2.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part2)));
+        binding.tv3.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part3)));
+        binding.tv4.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part4)));
+        binding.tv5.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part5)));
+        binding.tv6.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part6)));
+        binding.tv7.setText(Html.fromHtml(getString(R.string.dialog_notice_content_part7)));
+
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.text_to_user)
                 .setView(binding.getRoot())
