19204b3e620630fe671c0a62767bacb44ce7bf3f
Wang Shouheng <shouheng2015@gmail.com>
2018-01-26 13:04:55 +0800

    modify widget provider

--- app/src/main/java/me/shouheng/notepal/config/Constants.java
+++ app/src/main/java/me/shouheng/notepal/config/Constants.java
@@ -57,6 +57,7 @@ public class Constants {
     public final static String ACTION_WIDGET_LIST = "action_widget_list";
     public final static String ACTION_TAKE_PHOTO = "action_widget_take_photo";
     public final static String ACTION_ADD_NOTE = "action_widget_add_note";
+    public final static String ACTION_ADD_MIND = "action_widget_add_mind";
     public final static String ACTION_WIDGET_LAUNCH_APP = "action_widget_launch_app";
 
     public final static String PREFS_NAME = BuildConfig.APPLICATION_ID + "_preferences";
