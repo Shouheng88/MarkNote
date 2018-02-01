d7cfde0fa7eefa12cb16b11ee19c4967a5e89bea
Wang Shouheng <shouheng2015@gmail.com>
2018-02-01 12:58:44 +0800

    format code

--- app/src/main/java/me/shouheng/notepal/fragment/BaseFragment.java
+++ app/src/main/java/me/shouheng/notepal/fragment/BaseFragment.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.databinding.ViewDataBinding;
 import android.graphics.Bitmap;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.webkit.WebView;
 
@@ -15,11 +14,9 @@ import org.polaric.colorful.PermissionUtils;
 import java.io.File;
 
 import me.shouheng.notepal.R;
-import me.shouheng.notepal.dialog.AttachmentPickerDialog;
 import me.shouheng.notepal.listener.OnAttachingFileListener;
 import me.shouheng.notepal.model.Attachment;
 import me.shouheng.notepal.util.AttachmentHelper;
-import me.shouheng.notepal.util.AttachmentHelper.OnGetAttachmentListener;
 import me.shouheng.notepal.util.FileHelper;
 import me.shouheng.notepal.util.ScreenShotHelper;
 import me.shouheng.notepal.util.ToastUtils;
@@ -147,25 +144,6 @@ public abstract class BaseFragment<V extends ViewDataBinding> extends CommonFrag
     // endregion
 
     // region attachment
-
-    /**
-     * Note of usage of this program structure:
-     * 1. These methods are called when you clicked some items on the {@link AttachmentPickerDialog}
-     * 2. When you finally selected some images or videos, the {@link #onActivityResult(int, int, Intent)}
-     * will be called first, in this method we called
-     * {@link AttachmentHelper#resolveResult(Fragment, int, Intent, OnGetAttachmentListener)}
-     * to resolve this event. When the logic is completed in this method, methods in {@link OnAttachingFileListener} will be called.
-     * That means {@link #onAttachingFileErrorOccurred(Attachment)} and {@link #onAttachingFileFinished(Attachment)}.
-     * 3. In these two methods will called {@link #onGetAttachment(Attachment)} and {@link #onFailedGetAttachment(Attachment)}
-     * to let the child fragment to resolve these events.
-     * 4. As for {@link #getAttachmentPickerDialog()}, it is necessary to get the uri of given image,
-     * So the child fragment must override this method, and return the attachment picker dialog.
-     *
-     * @return given attachment picker
-     */
-    protected AttachmentPickerDialog getAttachmentPickerDialog() {
-        return null;
-    }
 
     protected void onGetAttachment(Attachment attachment) {}
 
