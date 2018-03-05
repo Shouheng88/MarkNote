d0aadf4a9ce47c90e1f69c78e148ff557c77b3d3
shouheng88 <shouheng2015@gmail.com>
2018-03-05 22:52:44 +0800

    fragment transaction error when activity is finishing

--- app/src/main/java/me/shouheng/notepal/util/FragmentHelper.java
+++ app/src/main/java/me/shouheng/notepal/util/FragmentHelper.java
@@ -13,23 +13,35 @@ import me.shouheng.notepal.R;
  * Created by wang shouheng on 2017/12/23.*/
 public class FragmentHelper {
 
-    public static void replace(AppCompatActivity activity, Fragment fragment, @IdRes int containerId) {
+    public static void replace(AppCompatActivity activity,
+                               Fragment fragment,
+                               @IdRes int containerId) {
         replace(activity, fragment, containerId, false);
     }
 
-    public static void replaceWithCallback(AppCompatActivity activity, Fragment fragment, @IdRes int containerId) {
+    public static void replaceWithCallback(AppCompatActivity activity,
+                                           Fragment fragment,
+                                           @IdRes int containerId) {
         replace(activity, fragment, containerId, true);
     }
 
-    public static void replace(AppCompatActivity activity, android.app.Fragment fragment, @IdRes int containerId) {
+    public static void replace(AppCompatActivity activity,
+                               android.app.Fragment fragment,
+                               @IdRes int containerId) {
         replace(activity, fragment, containerId, false);
     }
 
-    public static void replaceWithCallback(AppCompatActivity activity, android.app.Fragment fragment, @IdRes int containerId) {
+    public static void replaceWithCallback(AppCompatActivity activity,
+                                           android.app.Fragment fragment,
+                                           @IdRes int containerId) {
         replace(activity, fragment, containerId, true);
     }
 
-    private static void replace(AppCompatActivity activity, android.app.Fragment fragment, @IdRes int containerId, boolean backStack) {
+    private static void replace(AppCompatActivity activity,
+                                android.app.Fragment fragment,
+                                @IdRes int containerId,
+                                boolean backStack) {
+        if (activity.isFinishing()) return;
         android.app.FragmentManager fragmentManager = activity.getFragmentManager();
         android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
         setCustomAnimations(transaction);
@@ -37,7 +49,11 @@ public class FragmentHelper {
         transaction.replace(containerId, fragment).commit();
     }
 
-    private static void replace(AppCompatActivity activity, Fragment fragment, @IdRes int containerId, boolean backStack) {
+    private static void replace(AppCompatActivity activity,
+                                Fragment fragment,
+                                @IdRes int containerId,
+                                boolean backStack) {
+        if (activity.isFinishing()) return;
         FragmentManager fragmentManager = activity.getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         setCustomAnimations(transaction);
@@ -47,11 +63,13 @@ public class FragmentHelper {
 
     private static void setCustomAnimations(FragmentTransaction transaction) {
         if (!PreferencesUtils.getInstance(PalmApp.getContext()).systemAnimationEnabled()) return;
-        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down, R.animator.slide_up, R.animator.slide_down);
+        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down,
+                R.animator.slide_up, R.animator.slide_down);
     }
 
     private static void setCustomAnimations(android.app.FragmentTransaction transaction) {
         if (!PreferencesUtils.getInstance(PalmApp.getContext()).systemAnimationEnabled()) return;
-        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down, R.animator.slide_up, R.animator.slide_down);
+        transaction.setCustomAnimations(R.animator.slide_up, R.animator.slide_down,
+                R.animator.slide_up, R.animator.slide_down);
     }
 }
