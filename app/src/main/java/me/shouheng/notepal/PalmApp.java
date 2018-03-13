bea6c55bc76c4fa7ca35cc2d38a925ab51aa9116
Wang Shouheng <shouheng2015@gmail.com>
2018-03-13 19:41:24 +0800

    add one todo item

--- app/src/main/java/me/shouheng/notepal/PalmApp.java
+++ app/src/main/java/me/shouheng/notepal/PalmApp.java
@@ -28,7 +28,8 @@ import me.shouheng.notepal.model.Model;
  * 12. Calendar + Timeline;
  * 13. Google map location info;
  * 14. Multiple platform statistics and user trace;
- * 15, Bug in LockActivity when screen orientation changed. (Multiple input and headers)
+ * 15, Bug in LockActivity when screen orientation changed. (Multiple input and headers);
+ * 16. Enable pick category with icons.
  *
  * Created by wangshouheng on 2017/2/26. */
 public class PalmApp extends Application {
