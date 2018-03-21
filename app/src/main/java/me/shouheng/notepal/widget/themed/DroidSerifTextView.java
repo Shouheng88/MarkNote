80dcfffea31b0541c0662061dda2cf4ed8c88863
Shouheng <w_shouheng@163.com>
2018-03-21 22:20:23 +0800

    add note expand list view

---
+++ app/src/main/java/me/shouheng/notepal/widget/themed/DroidSerifTextView.java
@@ -0,0 +1,30 @@
+package me.shouheng.notepal.widget.themed;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * Created by shouh on 2018/3/21.*/
+public class DroidSerifTextView extends SupportTextView {
+
+    public DroidSerifTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DroidSerifTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DroidSerifTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "DroidSerif-Regular.ttf");
+        setTypeface(typeface);
+    }
+}
