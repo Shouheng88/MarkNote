c30354f5f5165af9a593d5a6738cb11f366a2777
Shouheng <w_shouheng@163.com>
2018-04-03 12:26:37 +0800

    add new scroll bar for note edit and view fragment

--- app/src/main/java/me/shouheng/notepal/fragment/NoteViewFragment.java
+++ app/src/main/java/me/shouheng/notepal/fragment/NoteViewFragment.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ActionMode;
@@ -30,6 +29,7 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import me.shouheng.notepal.PalmApp;
 import me.shouheng.notepal.R;
 import me.shouheng.notepal.activity.ContentActivity;
 import me.shouheng.notepal.config.Constants;
@@ -146,9 +146,9 @@ public class NoteViewFragment extends BaseFragment<FragmentNoteViewBinding> {
     }
 
     private void configViews() {
-        getBinding().mdView.getDelegate().setThumbDrawable(ContextCompat.getDrawable(getContext(),
-                R.drawable.recyclerview_fastscroller_handle));
-        getBinding().mdView.getDelegate().setThumbSize(8, 32);
+        getBinding().mdView.getDelegate().setThumbDrawable(PalmApp.getDrawableCompact(
+                isDarkTheme() ? R.drawable.fast_scroll_bar_dark : R.drawable.fast_scroll_bar_light));
+        getBinding().mdView.getDelegate().setThumbSize(16, 40);
         getBinding().mdView.getDelegate().setThumbDynamicHeight(false);
         getBinding().mdView.setHtmlResource(isDarkTheme());
         getBinding().mdView.parseMarkdown(note.getContent());
