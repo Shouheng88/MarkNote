284e32e8722ba281f76ef2e19907de33e6052c39
WngShhng <shouheng2015@gmail.com>
2018-04-06 10:31:57 +0800

    add SynchronizeUtils

--- app/src/main/java/me/shouheng/notepal/util/PalmUtils.java
+++ app/src/main/java/me/shouheng/notepal/util/PalmUtils.java
@@ -1,17 +1,8 @@
 package me.shouheng.notepal.util;
 
-import android.app.Activity;
 import android.os.Build;
-import android.text.TextUtils;
-
-import com.onedrive.sdk.core.ClientException;
 
 import me.shouheng.notepal.PalmApp;
-import me.shouheng.notepal.R;
-import me.shouheng.notepal.activity.SettingsActivity;
-import me.shouheng.notepal.async.onedrive.OneDriveBackupService;
-import me.shouheng.notepal.manager.onedrive.DefaultCallback;
-import me.shouheng.notepal.manager.onedrive.OneDriveManager;
 
 /**
  * Created by wang shouheng on 2017/12/23.*/
@@ -63,42 +54,5 @@ public class PalmUtils {
 
     public static String getPackageName(){
         return PalmApp.getContext().getApplicationContext().getPackageName();
-    }
-
-    /**
-     * Sync to One Drive.
-     *
-     * @param activity current activity */
-    public static void syncOneDrive(Activity activity) {
-        syncOneDrive(activity, 0, false);
-    }
-
-    /**
-     * Sync to One Drive
-     *
-     * @param activity current activity
-     * @param req the request code for opening setting activity
-     * @param force true to goto setting, else return */
-    public static void syncOneDrive(Activity activity, int req, boolean force) {
-        String itemId = PreferencesUtils.getInstance().getOneDriveBackupItemId();
-        String filesItemId = PreferencesUtils.getInstance().getOneDriveFilesBackupItemId();
-        if ((TextUtils.isEmpty(itemId) || TextUtils.isEmpty(filesItemId)) && force) {
-            ToastUtils.makeToast(R.string.login_drive_message);
-            SettingsActivity.start(activity, SettingsActivity.ACTION_NAV_TO_BACKUP_FRAGMENT, req);
-            return;
-        }
-
-        OneDriveManager.getInstance().connectOneDrive(activity, new DefaultCallback<Void>(activity) {
-            @Override
-            public void success(Void aVoid) {
-                OneDriveBackupService.start(activity);
-                ToastUtils.makeToast(R.string.text_syncing);
-            }
-
-            @Override
-            public void failure(ClientException error) {
-                super.failure(error);
-            }
-        });
     }
 }
