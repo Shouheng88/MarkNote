4bd16e06b84a93da8022b90f2acfb7faa2c95717
WngShhng <shouheng2015@gmail.com>
2018-04-06 13:50:13 +0800

    modify file upload task

--- app/src/main/java/me/shouheng/notepal/async/onedrive/FileUploadTask.java
+++ app/src/main/java/me/shouheng/notepal/async/onedrive/FileUploadTask.java
@@ -15,10 +15,15 @@ public class FileUploadTask extends AsyncTask<File, Integer, String> {
 
     private String itemId;
 
+    private ConflictBehavior conflictBehavior;
+
     private OneDriveManager.UploadProgressCallback<Item> uploadProgressCallback;
 
-    FileUploadTask(String itemId, OneDriveManager.UploadProgressCallback<Item> uploadProgressCallback) {
+    FileUploadTask(String itemId,
+                   ConflictBehavior conflictBehavior,
+                   OneDriveManager.UploadProgressCallback<Item> uploadProgressCallback) {
         this.itemId = itemId;
+        this.conflictBehavior = conflictBehavior;
         this.uploadProgressCallback = uploadProgressCallback;
     }
 
@@ -26,7 +31,7 @@ public class FileUploadTask extends AsyncTask<File, Integer, String> {
     protected String doInBackground(File... files) {
         LogUtils.d(files.length);
         for (File file : files) {
-            OneDriveManager.getInstance().upload(itemId, file, uploadProgressCallback);
+            OneDriveManager.getInstance().upload(itemId, file, conflictBehavior, uploadProgressCallback);
         }
         return "executed";
     }
