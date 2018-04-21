e4e2331f27a1535adc5f450f442bf32f8985b33f
WngShhng <shouheng2015@gmail.com>
2018-04-21 23:10:35 +0800

    modify weather fields

--- app/src/main/java/me/shouheng/notepal/model/Weather.java
+++ app/src/main/java/me/shouheng/notepal/model/Weather.java
@@ -3,16 +3,17 @@ package me.shouheng.notepal.model;
 import me.shouheng.notepal.model.enums.WeatherType;
 import me.shouheng.notepal.provider.annotation.Column;
 import me.shouheng.notepal.provider.annotation.Table;
+import me.shouheng.notepal.provider.schema.WeatherSchema;
 
 /**
  * Created by shouh on 2018/3/19.*/
-@Table(name = "gt_weather")
+@Table(name = WeatherSchema.TABLE_NAME)
 public class Weather extends Model {
 
-    @Column(name = "weather_type")
+    @Column(name = WeatherSchema.WEATHER_TYPE)
     private WeatherType type;
 
-    @Column(name = "temperature")
+    @Column(name = WeatherSchema.TEMPERATURE)
     private int temperature;
 
     public WeatherType getType() {
