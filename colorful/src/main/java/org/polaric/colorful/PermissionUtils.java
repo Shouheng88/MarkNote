e0e28f3e85724a1dd0f2ec08cf75437fba59a62a
Wang Shouheng <shouheng2015@gmail.com>
2018-06-01 16:59:06 +0800

    refactor permission request codes

--- colorful/src/main/java/org/polaric/colorful/PermissionUtils.java
+++ colorful/src/main/java/org/polaric/colorful/PermissionUtils.java
@@ -14,51 +14,41 @@ import android.support.v4.content.ContextCompat;
  * Created by wang shouheng on 2017/12/5.*/
 public class PermissionUtils {
 
-    private static final int REQUEST_PERMISSION_STORAGE = 10005;
-    private static final int REQUEST_PERMISSION_PHONE_STATE = 10006;
-    private static final int REQUEST_PERMISSION_LOCATION = 10007;
-    private static final int REQUEST_PERMISSION_MICROPHONE = 10008;
-    private static final int REQUEST_PERMISSION_SMS = 10009;
-    private static final int REQUEST_PERMISSION_SENSORS = 10010;
-    private static final int REQUEST_PERMISSION_CONTACTS = 10011;
-    private static final int REQUEST_PERMISSION_CAMERA = 10012;
-    private static final int REQUEST_PERMISSION_CALENDAR = 10013;
-
     public static <T extends BaseActivity> void checkStoragePermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_PERMISSION_STORAGE, callback);
+        checkPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE, PermissionRequestCode.REQUEST_PERMISSION_STORAGE, callback);
     }
 
     public static <T extends BaseActivity> void checkPhonePermission(@NonNull T activity, OnGetPermissionCallback callback){
-        checkPermission(activity, Manifest.permission.READ_PHONE_STATE, REQUEST_PERMISSION_PHONE_STATE, callback);
+        checkPermission(activity, Manifest.permission.READ_PHONE_STATE, PermissionRequestCode.REQUEST_PERMISSION_PHONE_STATE, callback);
     }
 
     public static <T extends BaseActivity> void checkLocationPermission(@NonNull T activity, OnGetPermissionCallback callback){
-        checkPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_LOCATION, callback);
+        checkPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION, PermissionRequestCode.REQUEST_PERMISSION_LOCATION, callback);
     }
 
     public static <T extends BaseActivity> void checkRecordPermission(@NonNull T activity, OnGetPermissionCallback callback){
-        checkPermission(activity, Manifest.permission.RECORD_AUDIO, REQUEST_PERMISSION_MICROPHONE, callback);
+        checkPermission(activity, Manifest.permission.RECORD_AUDIO, PermissionRequestCode.REQUEST_PERMISSION_MICROPHONE, callback);
     }
 
     public static <T extends BaseActivity> void checkSmsPermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.SEND_SMS, REQUEST_PERMISSION_SMS, callback);
+        checkPermission(activity, Manifest.permission.SEND_SMS, PermissionRequestCode.REQUEST_PERMISSION_SMS, callback);
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT_WATCH)
     public static <T extends BaseActivity> void checkSensorsPermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.BODY_SENSORS, REQUEST_PERMISSION_SENSORS, callback);
+        checkPermission(activity, Manifest.permission.BODY_SENSORS, PermissionRequestCode.REQUEST_PERMISSION_SENSORS, callback);
     }
 
     public static <T extends BaseActivity> void checkContactsPermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.READ_CONTACTS, REQUEST_PERMISSION_CONTACTS, callback);
+        checkPermission(activity, Manifest.permission.READ_CONTACTS, PermissionRequestCode.REQUEST_PERMISSION_CONTACTS, callback);
     }
 
     public static <T extends BaseActivity> void checkCameraPermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.CAMERA, REQUEST_PERMISSION_CAMERA, callback);
+        checkPermission(activity, Manifest.permission.CAMERA, PermissionRequestCode.REQUEST_PERMISSION_CAMERA, callback);
     }
 
     public static <T extends BaseActivity> void checkCalendarPermission(@NonNull T activity, OnGetPermissionCallback callback) {
-        checkPermission(activity, Manifest.permission.READ_CALENDAR, REQUEST_PERMISSION_CALENDAR, callback);
+        checkPermission(activity, Manifest.permission.READ_CALENDAR, PermissionRequestCode.REQUEST_PERMISSION_CALENDAR, callback);
     }
 
     private static <T extends BaseActivity> void checkPermission(@NonNull T activity, @NonNull String permission, int requestCode, OnGetPermissionCallback callback) {
@@ -74,23 +64,23 @@ public class PermissionUtils {
 
     static String getPermissionName(Context context, int requestCode) {
         switch (requestCode){
-            case REQUEST_PERMISSION_STORAGE:
+            case PermissionRequestCode.REQUEST_PERMISSION_STORAGE:
                 return context.getString(R.string.permission_storage_permission);
-            case REQUEST_PERMISSION_LOCATION:
+            case PermissionRequestCode.REQUEST_PERMISSION_LOCATION:
                 return context.getString(R.string.permission_location_permission);
-            case REQUEST_PERMISSION_MICROPHONE:
+            case PermissionRequestCode.REQUEST_PERMISSION_MICROPHONE:
                 return context.getString(R.string.permission_microphone_permission);
-            case REQUEST_PERMISSION_PHONE_STATE:
+            case PermissionRequestCode.REQUEST_PERMISSION_PHONE_STATE:
                 return context.getString(R.string.permission_phone_permission);
-            case REQUEST_PERMISSION_SMS:
+            case PermissionRequestCode.REQUEST_PERMISSION_SMS:
                 return context.getString(R.string.permission_sms_permission);
-            case REQUEST_PERMISSION_SENSORS:
+            case PermissionRequestCode.REQUEST_PERMISSION_SENSORS:
                 return context.getString(R.string.permission_sensor_permission);
-            case REQUEST_PERMISSION_CONTACTS:
+            case PermissionRequestCode.REQUEST_PERMISSION_CONTACTS:
                 return context.getString(R.string.permission_contacts_permission);
-            case REQUEST_PERMISSION_CAMERA:
+            case PermissionRequestCode.REQUEST_PERMISSION_CAMERA:
                 return context.getString(R.string.permission_camera_permission);
-            case REQUEST_PERMISSION_CALENDAR:
+            case PermissionRequestCode.REQUEST_PERMISSION_CALENDAR:
                 return context.getString(R.string.permission_calendar_permission);
         }
         return context.getString(R.string.permission_default_permission_name);
