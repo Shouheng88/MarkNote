9bd633ec210ea3205cb48d6341b1a35bbff79fae
WngShhng <shouheng2015@gmail.com>
2018-11-19 22:38:00 +0800

    add container activity

--- commons/src/main/java/me/shouheng/commons/activity/ContainerActivity.java
+++ commons/src/main/java/me/shouheng/commons/activity/ContainerActivity.java
@@ -1,97 +1,31 @@
-package cn.glority.receipt.view.common;
+package me.shouheng.commons.activity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
 
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.glority.commons.router.Router;
-import com.test.generatedAPI.API.enums.PaymentType;
+import me.shouheng.commons.R;
+import me.shouheng.commons.databinding.ActivityContainerBinding;
+import me.shouheng.commons.fragment.FragmentHelper;
+import me.shouheng.commons.utils.BackEventResolver;
+import me.shouheng.commons.utils.ColorUtils;
+import me.shouheng.commons.utils.FragmentKeyDown;
+import me.shouheng.commons.utils.PalmUtils;
 
-import cn.glority.receipt.R;
-import cn.glority.receipt.common.activity.CommonDaggerActivity;
-import cn.glority.receipt.common.config.RequestCode;
-import cn.glority.receipt.common.util.FragmentHelper;
-import cn.glority.receipt.databinding.ActivityContainerBinding;
-import cn.glority.receipt.view.account.AboutUsFragment;
-import cn.glority.receipt.view.account.ChangeEmailFragment;
-import cn.glority.receipt.view.account.ChangePhoneFragment;
-import cn.glority.receipt.view.account.FeedbackFragment;
-import cn.glority.receipt.view.account.PersonInfoFragment;
-import cn.glority.receipt.view.account.TermServiceFragment;
-import cn.glority.receipt.view.corporate.CorporateFragment;
-import cn.glority.receipt.view.main.interaction.BackEventResolver;
+public class ContainerActivity extends CommonActivity<ActivityContainerBinding> {
 
-public class ContainerActivity extends CommonDaggerActivity<ActivityContainerBinding> {
+    public final static String ACTION_OPEN_FRAGMENT = "__action_open_fragment";
+    public final static String ACTION_OPEN_FRAGMENT_EXTRA_NEED_TOOLBAR = "__action_open_fragment_extra_need_toolbar";
+    public final static String ACTION_OPEN_FRAGMENT_EXTRA_CLASS = "__action_open_fragment_extra_class";
+    public final static String ACTION_OPEN_FRAGMENT_EXTRA_BUNDLE = "__action_open_fragment_extra_bundle";
 
-    private final static String ACTION_CHANGE_EMAIL = "__action_change_email";
-
-    private final static String ACTION_CHANGE_PHONE = "__action_change_phone";
-
-    private final static String ACTION_TERM_SERVICE = "__action_term_of_service";
-
-    private final static String ACTION_ABOUT_US = "__action_about_us";
-
-    private final static String ACTION_CORPORATE_PAGE = "__action_corporation_page";
-
-    private final static String ACTION_FEEDBACK_PAGE = "__action_feedback_page";
-
-    private final static String ACTION_PERSON_INFORMATION = "__action_person_info";
-
-    private final static String ACTION_PROJECT_PICKER = "__action_project_picker";
-    private final static String ACTION_PROJECT_PICKER_ARG_KEY_PAYMENT = "__project_picker_key_payment";
-
-    private final static String EXTRA_KEY_SHOW_RATE_APP = "__extra_key_show_rate_app_dialog";
-
-    public static void changeEmail(Fragment fragment, boolean showRateApp) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_CHANGE_EMAIL);
-        intent.putExtra(EXTRA_KEY_SHOW_RATE_APP, showRateApp);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_CHANGE_EMAIL);
-    }
-
-    public static void changePhone(Fragment fragment) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_CHANGE_PHONE);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_CHANGE_PHONE);
-    }
-
-    public static void termService(Activity activity) {
-        Intent intent = new Intent(activity, ContainerActivity.class);
-        intent.setAction(ACTION_TERM_SERVICE);
-        activity.startActivityForResult(intent, RequestCode.REQUEST_TERM_SERVICE);
-    }
-
-    public static void aboutUs(Fragment fragment) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_ABOUT_US);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_ABOUT_US);
-    }
-
-    public static void corporate(Activity activity) {
-        Intent intent = new Intent(activity, ContainerActivity.class);
-        intent.setAction(ACTION_CORPORATE_PAGE);
-        activity.startActivityForResult(intent, RequestCode.REQUEST_CORPORATE_PAGE);
-    }
-
-    public static void feedback(Fragment fragment) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_FEEDBACK_PAGE);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_FEEDBACK_PAGE);
-    }
-
-    public static void personInfo(Fragment fragment) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_PERSON_INFORMATION);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_PERSON_INFO);
-    }
-
-    public static void projectPicker(Fragment fragment, PaymentType paymentType) {
-        Intent intent = new Intent(fragment.getContext(), ContainerActivity.class);
-        intent.setAction(ACTION_PROJECT_PICKER);
-        intent.putExtra(ACTION_PROJECT_PICKER_ARG_KEY_PAYMENT, paymentType);
-        fragment.startActivityForResult(intent, RequestCode.REQUEST_PROJECT_PICKER);
+    public static <T extends Fragment> FragmentHelper.Builder<T> open(Class<T> withClz) {
+        return new FragmentHelper.Builder<>(withClz);
     }
 
     @Override
@@ -103,42 +37,71 @@ public class ContainerActivity extends CommonDaggerActivity<ActivityContainerBin
     protected void doCreateView(Bundle savedInstanceState) {
         Intent intent = getIntent();
         String action = intent.getAction();
-        if (ACTION_CHANGE_EMAIL.equals(action)) {
-            boolean showRateApp = intent.getBooleanExtra(EXTRA_KEY_SHOW_RATE_APP, false);
-            toFragment(ChangeEmailFragment.newInstance(showRateApp));
-        } else if (ACTION_CHANGE_PHONE.equals(action)) {
-            toFragment(ChangePhoneFragment.newInstance());
-        } else if (ACTION_TERM_SERVICE.equals(action)) {
-            toFragment(TermServiceFragment.newInstance());
-        } else if (ACTION_ABOUT_US.equals(action)) {
-            toFragment(AboutUsFragment.newInstance());
-        } else if (ACTION_CORPORATE_PAGE.equals(action)) {
-            toFragment(CorporateFragment.newInstance());
-        } else if (ACTION_FEEDBACK_PAGE.equals(action)) {
-            toFragment(FeedbackFragment.newInstance());
-        } else if (ACTION_PERSON_INFORMATION.equals(action)) {
-            toFragment(PersonInfoFragment.newInstance());
-        } else if (ACTION_PROJECT_PICKER.equals(action)) {
-            PaymentType paymentType = (PaymentType) intent.getSerializableExtra(ACTION_PROJECT_PICKER_ARG_KEY_PAYMENT);
-            Fragment fragment = (Fragment) ARouter.getInstance()
-                    .build(Router.FRAGMENT_PROJECT_PICKER)
-                    .withSerializable(Router.FRAGMENT_PROJECT_PICKER_ARG_PAYMENT_TYPE, paymentType)
-                    .navigation();
-            toFragment(fragment);
+        if (ACTION_OPEN_FRAGMENT.equals(action)){
+            Class<Fragment> fragmentClass = (Class<Fragment>) intent.getSerializableExtra(ACTION_OPEN_FRAGMENT_EXTRA_CLASS);
+            Bundle bundle = intent.getParcelableExtra(ACTION_OPEN_FRAGMENT_EXTRA_BUNDLE);
+            setupToolbar(savedInstanceState);
+            try {
+                Fragment fragment = fragmentClass.newInstance();
+                fragment.setArguments(bundle);
+                toFragment(fragment);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setupToolbar(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        if (!intent.hasExtra(ACTION_OPEN_FRAGMENT_EXTRA_NEED_TOOLBAR)
+                || intent.getBooleanExtra(ACTION_OPEN_FRAGMENT_EXTRA_NEED_TOOLBAR, true)) {
+            setSupportActionBar(getBinding().toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setHomeAsUpIndicator(ColorUtils.tintDrawable(
+                        PalmUtils.getDrawableCompact(R.drawable.ic_arrow_back_black_24dp),
+                        getThemeStyle().isDarkTheme ? Color.WHITE : Color.BLACK));
+            }
+            getBinding().toolbar.setTitleTextColor(getThemeStyle().isDarkTheme ? Color.WHITE : Color.BLACK);
+            if (getThemeStyle().isDarkTheme) {
+                getBinding().toolbar.setPopupTheme(R.style.AppTheme_PopupOverlayDark);
+            }
+        } else {
+            getBinding().barLayout.setVisibility(View.GONE);
         }
     }
 
     private void toFragment(Fragment fragment) {
-        FragmentHelper.replace(this, fragment,  R.id.fragment_container);
+        FragmentHelper.replace(this, fragment,  R.id.fragment_container, false);
     }
 
     @Override
-    public void onBackPressed() {
-        Fragment fragment = getCurrentFragment(R.id.fragment_container);
-        if (fragment != null && fragment instanceof BackEventResolver) {
-            ((BackEventResolver) fragment).resolveBackEvent();
-        } else {
-            super.onBackPressed();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                superOnBackPressed();
+                break;
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Fragment fragment = getCurrentFragment(R.id.fragment_container);
+        if (fragment instanceof FragmentKeyDown) {
+            if (((FragmentKeyDown) fragment).onFragmentKeyDown(keyCode, event)) {
+                return true;
+            } else {
+                return super.onKeyDown(keyCode, event);
+            }
+        }
+        if (fragment instanceof BackEventResolver) {
+            ((BackEventResolver) fragment).resolve();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ commons/src/main/java/me/shouheng/commons/fragment/WebviewFragment.java
@@ -0,0 +1,159 @@
+package me.shouheng.commons.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+
+import com.facebook.stetho.common.LogUtil;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.DefaultWebClient;
+
+import me.shouheng.commons.R;
+import me.shouheng.commons.databinding.FragmentWebviewBinding;
+import me.shouheng.commons.utils.FragmentKeyDown;
+import me.shouheng.commons.utils.IntentUtils;
+import me.shouheng.commons.utils.PalmUtils;
+
+/**
+ * @author shouh
+ * @version $Id: WebviewFragment, v 0.1 2018/11/17 20:40 shouh Exp$
+ */
+public class WebviewFragment extends CommonFragment<FragmentWebviewBinding> implements FragmentKeyDown {
+
+    public final static String ARGUMENT_KEY_URL = "__extra_key_url";
+    public final static String ARGUMENT_KEY_TITLE = "__extra_key_title";
+    public final static String ARGUMENT_KEY_USE_PAGE_TITLE = "__extra_use_page_title";
+
+    private AgentWeb mAgentWeb;
+    private boolean usePageTitle;
+    private PopupMenu mPopupMenu;
+    private String url;
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_webview;
+    }
+
+    @Override
+    protected void doCreateView(Bundle savedInstanceState) {
+        Bundle arguments = getArguments();
+        assert arguments != null;
+        url = arguments.getString(ARGUMENT_KEY_URL);
+        String title = arguments.getString(ARGUMENT_KEY_TITLE);
+        if (getActivity() != null) {
+            ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+            if (ab != null) ab.setTitle(title);
+        }
+        usePageTitle = arguments.getBoolean(ARGUMENT_KEY_USE_PAGE_TITLE);
+
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent(getBinding().llContainer, -1, new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+                .useDefaultIndicator(accentColor(), 3)
+                .setWebChromeClient(mWebChromeClient)
+                .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+                .setMainFrameErrorView(R.layout.agentweb_error_page, -1)
+                .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.DISALLOW)
+                .interceptUnkownUrl()
+                .createAgentWeb()
+                .ready().go(url);
+
+    }
+
+    protected WebChromeClient mWebChromeClient = new WebChromeClient() {
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            LogUtil.d("onProgressChanged:" + newProgress + "  view:" + view);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            if (usePageTitle) {
+                if (!TextUtils.isEmpty(title)) {
+                    if (title.length() > 10) {
+                        title = title.substring(0, 10).concat("...");
+                    }
+                }
+                if (getActivity() != null) {
+                    ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+                    if (ab != null) ab.setTitle(title);
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_web, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item_more) {
+            if (mPopupMenu == null) {
+                mPopupMenu = new PopupMenu(this.getActivity(), getBinding().v);
+                mPopupMenu.inflate(R.menu.menu_web_items);
+                mPopupMenu.setOnMenuItemClickListener(item1 -> {
+                    if (item1.getItemId() == R.id.item_copy) {
+                        PalmUtils.copy(getActivity(), url);
+                        return true;
+                    } else if (item1.getItemId() == R.id.item_open) {
+                        IntentUtils.openWebPage(getContext(), url);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            mPopupMenu.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        mAgentWeb.getWebLifeCycle().onResume();//恢复
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mAgentWeb.getWebLifeCycle().onPause();
+        super.onPause();
+    }
+
+    @Override
+    public boolean onFragmentKeyDown(int keyCode, KeyEvent event) {
+        return mAgentWeb.handleKeyEvent(keyCode, event);
+    }
+
+    @Override
+    public void onDestroyView() {
+        mAgentWeb.getWebLifeCycle().onDestroy();
+        super.onDestroyView();
+    }
+
+    @Override
+    protected String umengPageName() {
+        return "Webview";
+    }
+}
