ab1f280f7d5d54782d63f8ccf2fe9f727a842d97
Wang Shouheng <shouheng2015@gmail.com>
2018-11-28 17:48:10 +0800

    remove colorful module

--- colorful/src/main/java/org/polaric/colorful/Defaults.java
+++
@@ -1,9 +0,0 @@
-package org.polaric.colorful;
-
-public class Defaults {
-    public final static Colorful.ThemeColor primaryColor = Colorful.ThemeColor.BLUE_GREY;
-    public final static Colorful.AccentColor accentColor = Colorful.AccentColor.LIGHT_BLUE_700;
-    public final static boolean trans = false;
-    public final static boolean darkTheme = false;
-    public final static boolean isColoredNavigation = false;
-}
