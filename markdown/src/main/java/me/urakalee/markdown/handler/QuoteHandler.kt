b0d76fabeba53f1adaf87763e270558b6092d2ba
Wang Shouheng <shouheng2015@gmail.com>
2018-11-28 17:49:40 +0800

    remove markdown module

--- markdown/src/main/java/me/urakalee/markdown/handler/QuoteHandler.kt
+++
@@ -1,9 +0,0 @@
-package me.urakalee.markdown.handler
-
-import me.urakalee.markdown.MarkHandler
-
-/**
- * @author Uraka.Lee
- */
-object QuoteHandler : MarkHandler {
-}
--- markdown/src/main/java/my/shouheng/palmmarkdown/listener/MarkdownParseListener.java
+++
@@ -1,8 +0,0 @@
-package my.shouheng.palmmarkdown.listener;
-
-/**
- * Created by shouh on 2018/3/25.*/
-public interface MarkdownParseListener {
-    void onStart();
-    void onEnd();
-}
